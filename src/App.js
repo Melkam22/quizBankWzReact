b034c7aa18b50438258715056d64f1a7efa1fcfe
Ashu <ashenshi@gmail.com>
2020-05-11 18:36:54 +0200

    quizServer fetched on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import QuizBank from './QuizBankServer/quizBank';
 
-function App() {
+class App extends Component{
+  state = {
+    quizBank: []
+  }
+quizFunction = ()=>{
+  QuizBank().then(question=>{
+    this.setState({
+      quizBank: question
+    })
+  })
+}
+componentDidMount(){
+  this.quizFunction()
+}
+
+  render(){
   return (
     <div className="App">
       <h1>QuizBank with React</h1>
+      {this.state.quizBank.length > 0 && this.state.quizBank.map((
+        {question, answers, correct, questionId})=> <p2>{question}</p2>
+      )}
     </div>
   );
+}
 }
 
 export default App;
