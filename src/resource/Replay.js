f938e6aa231ba3e6589dca2adf2e93d5585962a5
Ashu <ashenshi@gmail.com>
2020-05-12 15:31:00 +0200

    rematch button added onReplay.js

--- src/resource/Replay.js
+++ src/resource/Replay.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../App.css';
 
-const Replay = ({score}) => (
+const Replay = ({score, rematch}) => (
     <div className="replayBox">  
     <div className="showScore">You have scored {score} / 10.</div>
+    <button className="rematch" onClick={rematch}>Replay</button>
     </div>
 )
 
