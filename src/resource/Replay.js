e967f69e0293154768ec272650eb45f20ab611f1
Ashu <ashenshi@gmail.com>
2020-05-12 16:00:28 +0200

    styled & complited

--- src/resource/Replay.js
+++ src/resource/Replay.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 const Replay = ({score, rematch}) => (
     <div className="replayBox">  
-    <div className="showScore">You have scored {score} / 10.</div>
+    <div className="showScore">You have scored {score} out of 10 questions.</div>
     <button className="rematch" onClick={rematch}>Replay</button>
     </div>
 )
